5b74f20a4f01309ac7d701c5f922ed34e54011c2
Sajid Hasan <85543838+sajid-ahnaf@users.noreply.github.com>
2023-09-23 11:52:08 +0600

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -1,23 +1,23 @@
-const logotext = "JOHN";
+const logotext = "SAJID";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "SAJID HASAN",
+    description: "I’m SAJID HASAN FrontEnd Engineer_ React devloper, currently working in EduTeach Industries",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m SAJID HASAN",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
-        third: "I develop mobile apps",
+        third: "I thrive on challenges",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "I am an enthusiastic coder! Currently job holder in reputed Singapore based EduTeach Company, BrightCHAMPS. I am a post graduate student of MIST. I have a Bachelor Degree in Electrical & Electronics Engineering. I am also a Math & Physics Instructor; Have 7+ years hands on expeiences. I can work independently using my own initiative or as part of a team. I like to take challenges, explore problems; solving them so i can lead a productive & super charming life. ",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
     title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    aboutme: "I am an enthusiastic coder! Currently job holder in reputed Singapore based EduTeach Company, BrightCHAMPS. I am a post graduate student of MIST. I have a Bachelor Degree in Electrical & Electronics Engineering. I am also a Math & Physics Instructor; Have 7+ years hands on expeiences. I can work independently using my own initiative or as part of a team. I like to take challenges, explore problems; solving them so i can lead a productive & super charming life. ",
 };
 const worktimeline = [{
         jobtitle: "Designer of week",
@@ -148,9 +148,9 @@ const contactConfig = {
 
 const socialprofils = {
     github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    facebook: "https://www.facebook.com/sajidhasanrawnak",
+    linkedin: "https://www.linkedin.com/in/sajid-hasan-91288a244",
+    twitter: "https://twitter.com/sajidhasan2019?t=VzGZFMg81tNREE2OCUBc0Q&s=09",
 };
 export {
     meta,
