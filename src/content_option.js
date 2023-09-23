6aa027b7144b56f4089259ce07fb409a5651e0be
Sajid Hasan <85543838+sajid-ahnaf@users.noreply.github.com>
2023-09-23 11:44:24 +0600

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -1,189 +1,23 @@
-const logotext = "SAJID";
+const logotext = "JOHN";
 const meta = {
-    title: "SAJID",
-    description: "I am an enthusiastic coder! Currently job holder in reputed Singapore based EduTeach Company, BrightCHAMPS. I am a post graduate student of MIST. I have a Bachelor Degree in Electrical & Electronics Engineering. I am also a Math & Physics Instructor; Have 7+ years hands on expeiences. I can work independently using my own initiative or as part of a team. I like to take challenges, explore problems; solving them so i can lead a productive & super charming life. ",
+    title: "John Doe",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
 };
 
 const introdata = {
-    title: "I’m SAJID HASAN",
+    title: "I’m John Doe",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
-        third: "I thrive on challenges",
+        third: "I develop mobile apps",
     },
-    description: "I am an enthusiastic coder!",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
     title: "abit about my self",
-    aboutme: "const logotext = "SAJID";
-const meta = {
-    title: "SAJID",
-    description: "I am an enthusiastic coder! Currently job holder in reputed Singapore based EduTeach Company, BrightCHAMPS. I am a post graduate student of MIST. I have a Bachelor Degree in Electrical & Electronics Engineering. I am also a Math & Physics Instructor; Have 7+ years hands on expeiences. I can work independently using my own initiative or as part of a team. I like to take challenges, explore problems; solving them so i can lead a productive & super charming life. ",
-};
-
-const introdata = {
-    title: "I’m SAJID HASAN",
-    animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I thrive on challenges",
-    },
-    description: "I am an enthusiastic coder!",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
-};
-
-const dataabout = {
-    title: "abit about my self",
-    aboutme: "I am an enthusiastic coder! Currently job holder in reputed Singapore based EduTeach Company, BrightCHAMPS. I am a post graduate student of MIST. I have a Bachelor Degree in Electrical & Electronics Engineering. I am also a Math & Physics Instructor; Have 7+ years hands on expeiences. I can work independently using my own initiative or as part of a team. I like to take challenges, explore problems; solving them so i can lead a productive & super charming life. ",
-};
-const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
-    },
-];
-
-const skills = [{
-        name: "Python",
-        value: 90,
-    },
-    {
-        name: "Djano",
-        value: 85,
-    },
-    {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "React",
-        value: 60,
-    },
-    {
-        name: "Jquery",
-        value: 85,
-    },
-];
-
-const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-];
-
-const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-];
-
-const contactConfig = {
-    YOUR_EMAIL: "sajidhasan2023@gmail.com",
-    YOUR_FONE: "+880 1575 342467",
-    description: "I am an enthusiastic coder! Currently job holder in reputed Singapore based EduTeach Company, BrightCHAMPS. I am a post graduate student of MIST. I have a Bachelor Degree in Electrical & Electronics Engineering. I am also a Math & Physics Instructor; Have 7+ years hands on expeiences. I can work independently using my own initiative or as part of a team. I like to take challenges, explore problems; solving them so i can lead a productive & super charming life. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
-};
-
-const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
-};
-export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
-};
-",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
 };
 const worktimeline = [{
         jobtitle: "Designer of week",
@@ -314,9 +148,9 @@ const contactConfig = {
 
 const socialprofils = {
     github: "https://github.com",
-    facebook: "https://www.facebook.com/sajidhasanrawnak",
-    linkedin: "https://www.linkedin.com/in/sajid-hasan-91288a244",
-    twitter: "https://twitter.com/sajidhasan2019?t=VzGZFMg81tNREE2OCUBc0Q&s=09",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
 };
 export {
     meta,
